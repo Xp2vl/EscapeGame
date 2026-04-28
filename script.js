5a130d6ce8aa894ddf769cfb4379ca7459f8ced5
Xp2vl <xp2vl@hotmail.com>
2026-04-28 20:27:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -397,7 +397,7 @@ flowSteps = [
     type: "dialog",
     run: () => playDialogControlled([
       { text: "Malthe: Yes, lad os så fange en Yeti.", sound: "Malthe_1.mp3" },
-      { text: "<br>Josephine: Kunne det være en ide at vi holder vagt?", sound: "josephine_1.mp3" },
+      { text: "<br>Josephine: Kunne det være en ide at vi holder vagt?", sound: "Josephine_1.mp3" },
       { text: "<br>Malthe: God ide Jose, oppe fra udkigsposten?", sound: null },
       { text: "<br>Josephine: Ja, vi kan sagtens hjælpe deroppefra.", sound: null },
       { text: "<br><br>Tag kort 2", sound: null }
